91bc18e37db90f7b36ab4c8aaa09620c390cbd84
tynan Sampsel <tynansampselr@gmail.com>
2024-03-13 20:05:40 -0300

    commit 2 - test getaddrinfo

--- main.c
+++ main.c
@@ -9,8 +9,28 @@
 #include <arpa/inet.h>
 #include <netdb.h>
 
-int main(){
-    //hi
+int main(int argumentsCount, char *arguements[]){
+    
+    //argument 1 is destination IP (ipv4)
+    //argument 2 is message
+    
+    //prepare address information
     struct addrinfo adinf, *linkedListP, *adinfP;
+    char service;
+    adinf->ai_socktype = SOCK_DGRAM
+
+    int a = getaddrinfo(arguements[1], "4950", adinf, &linkedListP)
+    if(a == 0){
+        printf("got something!!!\n")
+
+
+
+    } else {
+        printf("womp womp\n")
+
+    }
+
+    //get addressinfo
+
     return 0;
 }
