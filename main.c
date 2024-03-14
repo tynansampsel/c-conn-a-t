2f177f9eb3a60bf2a327b6e966e5fb2f169946f9
tynan Sampsel <tynansampselr@gmail.com>
2024-03-13 21:34:51 -0300

    commit 4

--- main.c
+++ main.c
@@ -15,6 +15,10 @@ int main(int argumentsCount, char *arguements[]){
     //argument 2 is message
     
     //prepare address information
+
+	int s;
+	int numbytes;
+
     struct addrinfo hints, *servinfo, *p;
     memset(&hints, 0, sizeof hints); // Initialize hints
     hints.ai_family = AF_INET; // IPv4
@@ -24,9 +28,40 @@ int main(int argumentsCount, char *arguements[]){
     int a = getaddrinfo(arguements[1], "4950", &hints, &servinfo);
     if(a == 0){
         printf("got something!!!\n");
+
+        for(p = servinfo; p!= NULL; p = p->ai_next){
+            s = socket(p->ai_family, p->ai_socktype,p->ai_protocol)
+
+            if(s > -1){
+                break;
+            }
+            printf("nah\n");
+            
+        }
+
+
     } else {
         printf("womp womp\n");
     }
+
+
+
+if (p == NULL) {
+		fprintf(stderr, "talker: failed to create socket\n");
+		return 2;
+	}
+
+	if ((numbytes = sendto(s, argv[2], strlen(argv[2]), 0,
+			 p->ai_addr, p->ai_addrlen)) == -1) {
+		perror("talker: sendto");
+		exit(1);
+	}
+
+	freeaddrinfo(servinfo);
+
+	printf("talker: sent %d bytes to %s\n", numbytes, argv[1]);
+	close(s);
+
 
     //get addressinfo
 
