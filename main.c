40af01a932bc45c4cbfa214784537a8a534e48f7
tynan Sampsel <tynansampselr@gmail.com>
2024-03-15 13:20:20 -0300

    p2 c1

--- main.c
+++ main.c
@@ -16,7 +16,7 @@ int main(int argumentsCount, char *arguements[]){
     
     //prepare address information
 
-	int s;
+	int sockfd;
 	int numbytes;
 
     struct addrinfo hints, *servinfo, *p;
@@ -30,9 +30,9 @@ int main(int argumentsCount, char *arguements[]){
         printf("got something!!!\n");
 
         for(p = servinfo; p != NULL; p = p->ai_next){
-            s = socket(p->ai_family, p->ai_socktype,p->ai_protocol);
+            sockfd = socket(p->ai_family, p->ai_socktype,p->ai_protocol);
 
-            if(s > -1){
+            if(sockfd > -1){
                 break;
             }
             printf("nah\n");
@@ -44,13 +44,65 @@ int main(int argumentsCount, char *arguements[]){
     numbytes = sendto(s, arguements[2], strlen(arguements[2]), 0, p->ai_addr, p->ai_addrlen);
 
 
-    freeaddrinfo(servinfo);
+    //freeaddrinfo(servinfo);
     printf("Sent!\n");
 
-    close(s);
+    //close(s);
 
 
+    //int sockfd;
+	struct addrinfo hints, *servinfo, *p;
+	//int rv;
+	//int numbytes;
+
+	struct sockaddr_storage their_addr;
+	char buf[100];
+	socklen_t addr_len;
+	char s[INET6_ADDRSTRLEN];
+
+    memset(&hints, 0, sizeof hints);
+    hints.ai_socktype = SOCK_DGRAM;
+    hints.ai_flags = AI_PASSIVE;
+
+    int r = getaddrinfo(NULL, "4950", &hints, &servinfo);
+
+    if (r == 0)
+    {
+        for(p = servinfo; p != NULL; p = p->ai_next){
+            sockfd = socket(p->ai_family, p->ai_socktype,p->ai_protocol);
+
+            if((sockfd) > -1){
+
+                int b = bind(sockfd, p->ai_addr, p->ai_addrlen);
+
+                break;
+            }
+            printf("nah\n");
+        }
+          
+
+        freeaddrinfo(servinfo);
+
+        addr_len = sizeof their_addr;
+        numbytes = recvfrom(sockfd, buf, 99 , 0, (struct sockaddr *)&their_addr, &addr_len);
+
+        printf("listener: got packet from %s\n",
+                inet_ntop(their_addr.ss_family,
+                    get_in_addr((struct sockaddr *)&their_addr),
+                    s, sizeof s));
+        printf("listener: packet is %d bytes long\n", numbytes);
+        buf[numbytes] = '\0';
+        printf("listener: packet contains \"%s\"\n", buf);
+
+        close(sockfd);
+    }
     //get addressinfo
 
     return 0;
+}
+
+
+void *get_in_addr(struct sockaddr *sa)
+{
+	return &(((struct sockaddr_in*)sa)->sin_addr);
 }
