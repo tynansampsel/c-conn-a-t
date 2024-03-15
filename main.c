2484ee25b8cca1a2f1fd8c4b247a95980c4d7823
tynan Sampsel <tynansampselr@gmail.com>
2024-03-15 14:06:38 -0300

    p2c13

--- main.c
+++ main.c
@@ -9,73 +9,53 @@
 #include <arpa/inet.h>
 #include <netdb.h>
 
-int main(int argumentsCount, char *arguements[]){
-    
-    //argument 1 is destination IP (ipv4)
-    //argument 2 is message
-    
-    //prepare address information
+int main(int argumentsCount, char *arguments[]) {
+    if (argumentsCount != 3) {
+        fprintf(stderr, "Usage: %s <destination IP> <message>\n", arguments[0]);
+        return 1;
+    }
 
-	int s;
-	int numbytes;
+    int s;
+    int numbytes;
 
     struct addrinfo hints, *servinfo, *p;
 
     memset(&hints, 0, sizeof hints); // Initialize hints
-    //hints.ai_family = AF_INET; // IPv4
-    //hints.ai_socktype = SOCK_DGRAM;
+    hints.ai_family = AF_UNSPEC; // Allow IPv4 or IPv6
+    hints.ai_socktype = SOCK_DGRAM; // Datagram socket
 
-    int a = getaddrinfo(arguements[1], "4950", &hints, &servinfo);
-    if(a == 0){
-        printf("got something!!!\n");
-
-        for(p = servinfo; p != NULL; p = p->ai_next){
-            printf("a-\n");
-
-            s = socket(p->ai_family, p->ai_socktype,p->ai_protocol);
-            printf("a0\n");
-
-            if(s > -1){
-                break;
-                printf("a1\n");
-
-            }
-            printf("nah\n");
-        }
-        printf("a2\n");
-
-    } else {
-        printf("womp womp\n");
+    int status = getaddrinfo(arguments[1], "4950", &hints, &servinfo);
+    if (status != 0) {
+        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(status));
+        return 1;
     }
-    printf("a3\n");
 
+    // Iterate through all the results and make a socket
+    for (p = servinfo; p != NULL; p = p->ai_next) {
+        s = socket(p->ai_family, p->ai_socktype, p->ai_protocol);
+        if (s == -1) {
+            perror("socket");
+            continue;
+        }
+        break; // If we successfully create a socket, exit the loop
+    }
 
     if (p == NULL) {
         fprintf(stderr, "Failed to create socket\n");
         return 1;
     }
 
-    numbytes = sendto(s, arguements[2], strlen(arguements[2]), 0, p->ai_addr, p->ai_addrlen);
+    // Send the message
+    numbytes = sendto(s, arguments[2], strlen(arguments[2]), 0, p->ai_addr, p->ai_addrlen);
     if (numbytes == -1) {
-        printf("n\n");
-
-		perror("talker: sendto");
-		exit(1);
-	} else {
-        printf("y\n");
-
+        perror("sendto");
+        return 1;
     }
 
-    printf("a4\n");
-
+    printf("Sent %d bytes to %s\n", numbytes, arguments[1]);
 
     freeaddrinfo(servinfo);
-    printf("Sent!\n");
-
     close(s);
-
-
-    //get addressinfo
 
     return 0;
 }
